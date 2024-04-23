f9ab5e1ad08bbe5230f25a8d493dc91caa05ec8c
Tomasz Osmanowski <tomasz.osmanowski@fujitsu.com>
2024-04-23 17:31:37 +0200

    added migrations

--- Z4U5Z/superHeroAPI-DotNet8/Program.cs
+++ Z4U5Z/superHeroAPI-DotNet8/Program.cs
@@ -17,6 +17,13 @@ builder.Services.AddDbContext<DataContext>(options =>
 
 var app = builder.Build();
 
+// Apply migrations
+using (var scope = app.Services.CreateScope())
+{
+    var dbContext = scope.ServiceProvider.GetRequiredService<DataContext>();
+    dbContext.Database.Migrate();
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
