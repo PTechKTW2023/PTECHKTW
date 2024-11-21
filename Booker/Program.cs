97caa22b3722e4f9eee2afcc3b4cc9adee35d60d
Tomek <t.osmanowski@gmail.com>
2024-11-21 21:58:27 +0100

    Booker inital config

---
+++ Booker/Program.cs
@@ -0,0 +1,34 @@
+using Microsoft.Extensions.Hosting.Internal;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
+
+// Add this block
+if (app.Environment.IsDevelopment())
+{
+    app.MapGet("/debug/routes", (IEnumerable<EndpointDataSource> endpointSources) =>
+        string.Join("\n", endpointSources.SelectMany(source => source.Endpoints)));
+}
+
+app.Run();
--- recyclingAPI/DTOs/WasteTypeDTO.cs
+++
@@ -1,9 +0,0 @@
-namespace recyclingAPI.DTOs
-{
-    public class WasteTypeDTO
-    {      
-        public string Name { get; set; }
-        public string Unit { get; set; }
-        public bool IsADR { get; set; }
-    }
-}
