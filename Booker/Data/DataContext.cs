b1766aad381f6af86e6e1697ebe8294d9fc0d8f1
Tomek <t.osmanowski@gmail.com>
2024-11-23 20:35:52 +0100

    database connection added

---
+++ Booker/Data/DataContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Booker.Data
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<TextBook> Books { get; set; }
+
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+    }
+}
---
+++ Booker/Data/TextBook.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Booker.Data
+{
+    public class TextBook
+    {
+        public int Id { get; set; }
+       
+        public string Title { get; set; }
+    }
+}
